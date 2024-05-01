138d14eb89cdc1ad190dc9393f99a55fab30f32f
LucasQuadra <lquadra703@gmail.com>
2024-05-01 15:01:23 -0300

    primeiro commit

---
+++ scrypt.js
@@ -0,0 +1,21 @@
+function toggleMode(){
+    const html= document.documentElement
+    html.classList.toggle('ligth')
+
+    // pegar a tag img
+    const img= document.querySelector("#profile img")
+    // substituir a imagem
+     if(html.classList.contains('ligth')) {
+    // se tiver ligth mode, adcionar a imagem ligth
+    img.setAttribute('src','__MACOSX/Avatar.png')
+    
+    }else{
+
+     }
+    // se tiver sem ligth mode, manter a imagem normal
+    img.setAttribute('src','__MACOSX/avatar-light.png')
+    
+
+}
+
+    
